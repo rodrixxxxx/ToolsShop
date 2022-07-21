e50d5e50c3401399d4b5f782f746419f440d57c6
Rodrigo Comesaña <rodry.roh@hotmail.com>
2022-07-21 17:52:38 -0300

    update3

--- js/contacto.js
+++ js/contacto.js
@@ -1,1 +1,8 @@
-document.getElementById("botonEnviarContacto").innerHTML 
+/*Hice un eventListener para que muestre un mensaje de confirmacion al clickear Enviar*/
+
+let boton = document.getElementById("botonenviar");     //CONECTO LA VARIABLE CON EL ELEMENTO DEL DOM
+boton.addEventListener("click", enviarNotificacion);    //CONECTO BOTON CON EL EVENTO Y LA FUNCTION
+
+function enviarNotificacion(){
+    document.getElementById("cartelEnviar").innerHTML = "Su consulta fue en viada con exito";  //MODIFICO EL "P" VACIO PARA QUE SE TRANSFORME EN EL MENSAJE
+}
